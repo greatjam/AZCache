397bddec12268a7ee760ce535f99c7bdcd4eb848
greatjam <alexzou0709@gmail.com>
2013-08-29 15:42:36 +0800

    Initial Commit

---
+++ AZCache/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AZCache
+//
+//  Created by Zou Alex on 8/29/13.
+//  Copyright (c) 2013 alex zou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AZCacheTests/AZCacheTests.m
@@ -0,0 +1,34 @@
+//
+//  AZCacheTests.m
+//  AZCacheTests
+//
+//  Created by Zou Alex on 8/29/13.
+//  Copyright (c) 2013 alex zou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AZCacheTests : XCTestCase
+
+@end
+
+@implementation AZCacheTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
